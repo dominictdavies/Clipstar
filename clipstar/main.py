b83894dc1ac94108509e961b89d88ecbbc1781f2
dominictdavies <dominictdavies@gmail.com>
2024-01-13 22:08:12 +0800

    Add string_recogniser and tests

--- clipstar/main.py
+++ clipstar/main.py
@@ -1,5 +1,13 @@
+def string_recogniser(string):
+    if type(string) != str:
+        raise TypeError()
+
+    return string + " <- That's a string!"
+
+
 def main():
     print("Hello world!")
+
 
 if __name__ == "__main__":
     main()
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,2 +1,20 @@
-def test_main():
-    assert True  # Placeholder assertion
+from clipstar.main import *
+import pytest
+
+
+def test_string_recogniser_string():
+    string = "This is a string."
+    output = string_recogniser(string)
+    assert output == string + " <- That's a string!"
+
+
+def test_string_recogniser_number():
+    number = 0
+    with pytest.raises(TypeError):
+        string_recogniser(number)
+
+
+def test_main(capsys):
+    main()
+    captured = capsys.readouterr()
+    assert captured.out == "Hello world!\n"
