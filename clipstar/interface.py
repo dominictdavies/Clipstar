2daca7d6c358f4a9b863ee1e9863a50de26ae7ef
dominictdavies <dominictdavies@gmail.com>
2024-02-10 00:20:10 +0800

    Add help

--- clipstar/interface.py
+++ clipstar/interface.py
@@ -3,3 +3,11 @@ This module is used for interfacing with Clipstar through the command line.
 
 
 """
+
+def execute_help(subject):
+    """Prints usage to the console."""
+    print(f"help {subject}")
+
+commands = {
+    "help": execute_help
+}
